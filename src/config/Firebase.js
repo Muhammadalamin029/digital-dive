0865fec045ce1081a32c932af42c34a22ca31d56
Muhammadalamin029 <mmuhammadalameen9@gmail.com>
2024-10-13 10:44:13 +0100

    Added firebase

---
+++ src/config/Firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCIKfWuTVDurw1obMs1ypnMuEySETV2lJA",
+  authDomain: "digital-dive.firebaseapp.com",
+  projectId: "digital-dive",
+  storageBucket: "digital-dive.appspot.com",
+  messagingSenderId: "676183035591",
+  appId: "1:676183035591:web:3a283a9b7af2d9729578e2",
+  measurementId: "G-KEREG44K89",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
