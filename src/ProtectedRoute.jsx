29e8e9a487d62944dd55b93aedfd2c0b59bddeb1
Muhammad Al-amin Musa <139736168+Muhammadalamin029@users.noreply.github.com>
2024-10-16 20:10:58 +0100

    Update ProtectedRoute.jsx

--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Outlet, Navigate } from "react-router-dom";
 
 const ProtectedRoute = () => {
-  const [user, setUser] = useState(false);
+  const [user, setUser] = useState(true);
 
   return user ? <Outlet /> : <Navigate to="/welcome" />;
 };
